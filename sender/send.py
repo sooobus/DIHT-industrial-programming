7782bb9085eb6f3710fc68e0bb4b2e26e6aca290
sooobus <sooobus@gmail.com>
2018-02-25 17:19:25 +0300

    cleared

--- sender/send.py
+++
@@ -1,7 +0,0 @@
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit', port=5672))
-channel = connection.channel()
-channel.queue_declare(queue='my_queue')
-channel.basic_publish(exchange='', routing_key='my_queue', body=input())
-connection.close()
